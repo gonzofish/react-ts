0bf5f8609378fc0ab05a015aaf0bce59b17be80b
Matt Fehskens <matt.fehskens@gmail.com>
2017-08-25 19:22:31 -0400

    Added TSLint to setup
    Required adding tslint-react as well

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -9,6 +9,6 @@ export interface HelloProps {
 //     <h1>Hello...from { props.compiler } and { props.framework }!</h1>;
 export class Hello extends React.Component<HelloProps, undefined> {
     render() {
-        return <h1>Hello...from { this.props.compiler } and { this.props.framework }!</h1>;
+        return <h1>Hello...from {this.props.compiler} and {this.props.framework}!</h1>;
     }
 }
