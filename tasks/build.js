7ffa5d3ffa428b4f5471b8d41e67b63f2bfe22cb
Matt Fehskens <matt.fehskens@gmail.com>
2017-08-26 17:21:48 -0400

    Rollup working
    Added main entrypoint `index.ts`

--- tasks/build.js
+++ tasks/build.js
@@ -11,14 +11,13 @@ const compile = () => Promise.all([2015, 5].map((type) =>
     tsc(root('configs', `tsconfig.es${ type }.json`))
 ));
 
-const root = (...args) =>
-    path.resolve.apply(
-        null,
-        [__dirname, '..'].concat(args)
-    );
-const libName = require(root('package.json')).name;
+const root = (...args) => path.resolve.apply(
+    null,
+    [__dirname, '..'].concat(args)
+);
+const pkg = require(root('package.json'));
 
-const _rollup = () => rollup(libName, {
+const _rollup = () => rollup(pkg.name, {
     dist: root('dist'),
     es2015: root('out-tsc', 'es2015'),
     es5: root('out-tsc', 'es5'),
@@ -27,7 +26,7 @@ const _rollup = () => rollup(libName, {
 
 return Promise.resolve()
     .then(compile)
-    .then(rollup)
+    .then(_rollup)
     .catch((error) => {
         console.error(error);
     });
--- tasks/rollup.js
+++ tasks/rollup.js
@@ -1,7 +1,102 @@
 'use strict';
 
-const execRollup = (libName, dirs) => {
+const fs = require('fs-extra');
+const path = require('path');
+const rollup = require('rollup');
+const rollupCommon = require('rollup-plugin-commonjs');
+const rollupNodeResolve = require('rollup-plugin-node-resolve');
+const rollupSourcemaps = require('rollup-plugin-sourcemaps');
+const rollupUglify = require('rollup-plugin-uglify');
 
+const execRollup = (name, dirs) => {
+    const destinations = generateDestinations(dirs.dist, name);
+    const es5Entry = path.resolve(dirs.es5, 'index.js');
+    const es2015Entry = path.resolve(dirs.es2015, 'index.js');
+    const baseConfig = generateConfig({
+        input: {
+            external: [
+                'react',
+                'react-dom'
+            ],
+            onwarn: function rollupOnWarn(warning) {
+                // keeps TypeScript this errors down
+                if (warning.code !== 'THIS_IS_UNDEFINED') {
+                    console.warn(warning.message);
+                }
+            },
+            plugins: [
+                rollupNodeResolve(),
+                rollupSourcemaps()
+            ]
+        },
+        output: {
+            globals: {
+                react: 'React',
+                'react-dom': 'ReactDOM'
+            },
+            name: dashToCamel(name),
+            sourcemap: true
+        }
+    }, dirs.root);
+
+    const es2015Config = mergeConfig(baseConfig, {
+        input: {
+            input: es2015Entry,
+
+        },
+        output: {
+            file: destinations.es2015,
+            format: 'es'
+        }
+    });
+    const es5Config = mergeConfig(baseConfig, {
+        input: {
+            input: es5Entry
+        },
+        output: {
+            file: destinations.es5,
+            format: 'es'
+        }
+    });
+    const umdConfig = mergeConfig(baseConfig, {
+        input: {
+            input: es5Entry,
+            plugins: baseConfig.input.plugins.concat([rollupUglify({})])
+        },
+        output: {
+            file: destinations.umd,
+            format: 'umd'
+        }
+    });
+
+    const bundles = [
+        es2015Config,
+        es5Config,
+        umdConfig
+    ].map((config) => rollup.rollup(config.input)
+        .then((bundle) => bundle.write(config.output))
+    );
+
+    return Promise.all(bundles);
 };
+
+const generateDestinations = (dist, moduleName) => Object.freeze({
+    es2015: path.resolve(dist, `${ moduleName }.js`),
+    es5: path.resolve(dist, `${ moduleName }.es5.js`),
+    umd: path.resolve(dist, `${ moduleName }.min.js`)
+});
+
+// placeholder?
+const generateConfig = (base, root) => base;
+
+const mergeConfig = (base, custom) => ({
+   input: Object.assign({}, base.input, custom.input || {}),
+   output: Object.assign({}, base.output, custom.output || {})
+});
+
+const dashToCamel = (value) =>
+    value.replace(/(-.)/g, (match) =>
+        match.replace('-', '').toUpperCase()
+    );
 
 module.exports = execRollup;
